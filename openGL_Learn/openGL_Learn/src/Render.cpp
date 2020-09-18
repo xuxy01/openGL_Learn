fcb626ef33ef0cb769f8134f10856238385376f0
xuxy <924763124@qq.com>
2020-09-18 17:33:59 +0800

    面剔除

--- openGL_Learn/openGL_Learn/src/Render.cpp
+++ openGL_Learn/openGL_Learn/src/Render.cpp
@@ -70,6 +70,8 @@ void Render::draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
 	Camera::getInstance()->update();
 	for (std::vector<Node*>::iterator iter = nodes.begin(); iter != nodes.end(); iter++)
